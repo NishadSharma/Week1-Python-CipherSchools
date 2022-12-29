f29fa470d27b3df44914b205f691369c91ed3ffd
Nishad <iamnishadsharma13@gmail.com>
2022-12-29 12:55:44 +0530

    python

---
+++ class18-varriables-cipherschools.py
@@ -0,0 +1,20 @@
+number1 = 1
+print(number1)
+number1 = 3 # reassigning
+print(number1)
+
+name = "Nishad"
+print(name)
+name = 555 # variables can be assigned any value, they are dynamic
+print(name)
+
+# 1user = "Name" variable cannot start with name
+# can use underscore at the start of the variabble
+
+_user2 = "Subhajeet"
+user1_rename = "Mr.Robot"
+
+print(_user2,user1_rename)
+
+user_one_name = "Robot" #Snake Case Styling
+UserOneName = "groot" #Camel Case
